042c0566d9ce6ddc0c0fa0028c471308ed039592
Ioanna Gogou <ioannagogou1998@gmail.com>
2020-08-28 00:54:44 +0300

    added fake graphs

--- js/user.js
+++ js/user.js
@@ -18,6 +18,34 @@ window.app = new Vue({
   el: '#loader'
 })
 
+var element = document.getElementById('progress_chart').getContext('2d');
+var chart = new Chart(element, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+      legend: {
+        display: false
+      },
+      responsive: {
+        responsive: false,
+        maintainAspectRatio: false
+      }
+    }
+});
+
 function hideLoader(){
   document.getElementById("loader").style.visibility = "hidden"
   document.getElementById("app").style.visibility = "visible"
