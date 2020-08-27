47907d4ce657aa015213bca64cf682ab45f37cce
xpthanos <49254999+xpthanos@users.noreply.github.com>
2020-08-27 19:05:39 +0300

    Temporary php fix

--- db/add_user_old.php
+++ db/add_user_old.php
@@ -7,24 +7,34 @@ $dbname = "userdata";
 $conn = mysqli_connect($host, $user, $password, $dbname);
 
 $result = array('error'=>false);
-echo $_POST['name'];
-echo $_POST['pass'];
-echo $_POST['email'];
+$action = '';
+if(isset($_GET['action']))
+{
+$action = $_GET['action'];
+}
+
+if($action == 'read'){
+$sql = $conn->query("SELECT * FROM user");
+$users = array();
+while($row = $sql->fetch_assoc())
+{
+array_push($users, $row);
+}
+$result['user'] = $users;
+}
+if($action == 'create'){
 $randid = rand(1,100000);
+$type = 'user';
+$sql = $conn->query("INSERT INTO user (userid,username,password,email,type) VALUES('$randid',$name','$pass', '$email', $type)");
 $name = $_POST['name'];
 $pass = $_POST['pass'];
 $email = $_POST['email'];
-$type = 1;
-$sql = $conn->query("INSERT INTO user (userid,username,password,email,type) VALUES('$randid','$name','$pass', '$email', '$type')");
-echo $sql;
-if($sql)
-{
 $users = array();
 while($row = $sql->fetch_assoc()){
 array_push($users, $row);
 }
 $result['users'] = $users;
-}
+
 
 if($sql){
 $result[ 'message'] = "User added successfully!";}
@@ -33,6 +43,6 @@ else{
 $result['error'] = true;
 $result['message'] = "Failed to add user!";
 }
-
+}
 echo json_encode($result);
 ?>
