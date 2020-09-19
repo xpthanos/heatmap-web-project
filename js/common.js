f3b7a864438db6c8c8e55587acafa5afa3941e3e
Panagiotis Baklesis <baklesis@users.noreply.github.com>
2020-09-19 22:55:31 +0300

    added upload functionality
    Used open-source library DropzoneJS

--- js/common.js
+++ js/common.js
@@ -198,6 +198,7 @@ window.app = new Vue({
 
   },
   methods: {
+
     showTab(sel_tab){
       //hide and show elements
       document.getElementById("overview").style.display = "none"
@@ -550,130 +551,7 @@ var year_chart = new Chart(year_canvas, {
     }
 })
 
-// Vue.component declaration for Upload:
 
-Vue.component('file-upload', {
-  template: '#upload',
-  data(){
-    return {
-      dragAndDropCapable: false,
-      files: [],
-      uploadPercentage: 0
-    }
-  },
-  mounted(){
-    /*
-      Determine if drag and drop functionality is capable in the browser
-    */
-    this.dragAndDropCapable = this.determineDragAndDropCapable();
-
-    /*
-      If drag and drop capable, then we continue to bind events to our elements.
-    */
-    if( this.dragAndDropCapable ){
-      /*
-        Listen to all of the drag events and bind an event listener to each
-        for the fileform.
-      */
-      ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop'].forEach( function( evt ) {
-        /*
-          For each event add an event listener that prevents the default action
-          (opening the file in the browser) and stop the propagation of the event (so
-          no other elements open the file in the browser)
-        */
-        this.$refs.fileform.addEventListener(evt, function(e){
-          e.preventDefault();
-          e.stopPropagation();
-        }.bind(this), false);
-      }.bind(this));
-
-      /*
-        Add an event listener for drop to the form
-      */
-      this.$refs.fileform.addEventListener('drop', function(e){
-        /*
-          Capture the files from the drop event and add them to our local files
-          array.
-        */
-        for( let i = 0; i < e.dataTransfer.files.length; i++ ){
-          this.files.push( e.dataTransfer.files[i] );
-        }
-      }.bind(this));
-    }
-  },
-
-  methods: {
-    /*
-      Determines if the drag and drop functionality is in the
-      window
-    */
-    determineDragAndDropCapable(){
-      /*
-        Create a test element to see if certain events
-        are present that let us do drag and drop.
-      */
-      var div = document.createElement('div');
-
-      /*
-        Check to see if the `draggable` event is in the element
-        or the `ondragstart` and `ondrop` events are in the element. If
-        they are, then we have what we need for dragging and dropping files.
-
-        We also check to see if the window has `FormData` and `FileReader` objects
-        present so we can do our AJAX uploading
-      */
-      return ( ( 'draggable' in div )
-              || ( 'ondragstart' in div && 'ondrop' in div ) )
-              && 'FormData' in window
-              && 'FileReader' in window;
-    },
-
-    /*
-      Submits the files to the server
-    */
-    submitFiles(){
-      /*
-        Initialize the form data
-      */
-      let formData = new FormData();
-
-      /*
-        Iteate over any file sent over appending the files
-        to the form data.
-      */
-      for( var i = 0; i < this.files.length; i++ ){
-        let file = this.files[i];
-
-        formData.append('files[' + i + ']', file);
-      }
-
-      /*
-        Make the request to the POST /file-drag-drop-instant URL
-      */
-      axios.post( '/file-drag-drop-instant',
-        formData,
-        {
-          headers: {
-              'Content-Type': 'multipart/form-data'
-          },
-          onUploadProgress: function( progressEvent ) {
-            this.uploadPercentage = parseInt( Math.round( ( progressEvent.loaded * 100 ) / progressEvent.total ) );
-          }.bind(this)
-        }
-      ).then(function(){
-        console.log('SUCCESS!!');
-      })
-      .catch(function(){
-        console.log('FAILURE!!');
-      });
-    }
-  }
-}
-)
-
-new Vue({ el: '#upload'})
-
-// Vue.component declaration over
 
 
 
