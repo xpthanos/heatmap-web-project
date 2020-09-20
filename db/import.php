41d40aebb5acaf1bb49907391a3b88f9534e0319
xpthanos <49254999+xpthanos@users.noreply.github.com>
2020-09-20 19:08:47 +0300

    Implement XML download plus other fixes

--- db/import.php
+++ db/import.php
@@ -68,7 +68,7 @@ foreach ($jsonStream as $name => $data) {
 	$activity_timestamp = intval($data['activity'][0]["timestampMs"]);
 	$activity_confidence =  intval($data['activity'][0]['activity'][0]["confidence"]);
 
-	$sql = $conn->query("INSERT INTO record(heading, activity_type, activity_confidence, activity_timestamp, vertical_accuracy, velocity, accuracy, longitude, latitude, altitude, record_timestamp, userid) VALUES ('$heading', '$activity_type', '$activity_confidence', FROM_UNIXTIME(0.001 * '$activity_timestamp'), '$vertical_accuracy', '$velocity', '$accuracy', '$longitude', '$latitude', '$altitude', FROM_UNIXTIME(0.001 * '$record_timestamp'), 0)");
+	$sql = $conn->query("INSERT INTO record(heading, activity_type, activity_confidence, activity_timestamp, vertical_accuracy, velocity, accuracy, longitude, latitude, altitude, record_timestamp, userid) VALUES ('$heading', '$activity_type', '$activity_confidence', FROM_UNIXTIME(0.001 * '$activity_timestamp'), '$vertical_accuracy', '$velocity', '$accuracy', '$longitude', '$latitude', '$altitude', NOW(), 0)");
 
 	if($sql){
 		echo ".";
